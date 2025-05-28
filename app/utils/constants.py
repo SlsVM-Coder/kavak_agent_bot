a4e52137180764572c189904cf91bc451b567332
Silas Valle <vallesilas@gmail.com>
2025-05-27 20:10:25 -0600

    [MacMini]: Create constant for final message

---
+++ app/utils/constants.py
@@ -0,0 +1,7 @@
+
+FAREWELL_MSG = (
+    "\n\nRecuerda que en Kavak queremos ayudarte a moverte libremente, "
+    "a crear tus propios recuerdos: esas salidas nocturnas con amigos coreando "
+    "tus canciones favoritas, quizá tu primer beso o simplemente disfrutar de la "
+    "ciudad a tu ritmo. Cuando sientas que es tu momento, aquí te esperamos. 🚗✨"
+)
